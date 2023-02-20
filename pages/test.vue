3ca2ccd42b1fec1e3125ce212fbe3bf37ff4a470
ManUtopiK <emmanuel.salomon@gmail.com>
2023-02-20 12:04:52 +0100

    added graphql test page

---
+++ pages/test.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useQuery } from 'villus'
+
+const getPhotos = `
+  query getPhotos {
+    photos: photo(limit: 10) {
+      id
+      content
+    }
+  }
+`
+
+const { data } = useQuery({
+  query: getPhotos,
+})
+</script>
+
+<template>
+  <div>
+    <ul v-if="data.photos">
+      <li v-for="(photo, i) of data.photos" :key="i">
+        {{ photo.id }}
+      </li>
+    </ul>
+  </div>
+</template>
