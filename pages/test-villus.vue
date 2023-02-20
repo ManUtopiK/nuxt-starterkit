51bb66f61ec477f180c81ad99a38c2e76d743187
ManUtopiK <emmanuel.salomon@gmail.com>
2023-02-20 15:42:02 +0100

    test villus

---
+++ pages/test-villus.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { useQuery } from 'villus'
+
+const hasuraSiteId = ref('1')
+const limitQuery = ref(10)
+const getPhotos = computed(() => {
+  return `
+  query getPhotos {
+    photos: photo(limit: ${limitQuery.value}) {
+      id
+      content
+    }
+  }
+`
+})
+
+const { data, pending, error, refresh } = await useLazyAsyncData(
+  'photos',
+  async () => {
+    const { execute, isFetching } = useQuery({
+      query: getPhotos,
+      context: {
+        headers: {
+          'x-hasura-site-id': hasuraSiteId.value,
+        },
+      },
+    })
+
+    await new Promise(r => setTimeout(r, 2000)) // 2s
+
+    const { data } = await execute()
+    return data
+  },
+)
+
+// const { execute, isFetching } = useQuery({
+//   query: getPhotos,
+//   fetchOnMount: false,
+//   context: {
+//     headers: {
+//       'x-hasura-site-id': hasuraSiteId.value,
+//     },
+//   },
+// })
+
+// const { data, pending, error, refresh } = await useAsyncData('photos', () => execute())
+</script>
+
+<template>
+  <div>
+    <nuxt-link to="/">
+      index
+    </nuxt-link>
+
+    <button @click="refresh()">
+      refresh
+    </button>
+
+    <ul v-if="!pending">
+      <li v-for="(photo, i) of data.photos" :key="i">
+        {{ photo.id }}
+      </li>
+    </ul>
+    <div v-else>
+      Fetching
+    </div>
+  </div>
+</template>
