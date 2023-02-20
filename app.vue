26f391a313a5a98d976edc50a691b0ca9b1feaca
ManUtopiK <emmanuel.salomon@gmail.com>
2023-02-20 12:01:53 +0100

    villus config

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script setup>
+import { defaultPlugins, useClient } from 'villus'
+
+function authPlugin({ opContext }) {
+  // opContext.headers.Authorization = 'Bearer <token>'
+  opContext.headers['x-hasura-site-id'] = 1
+}
+
+useClient({
+  url: 'https://graphql.dev.97px.fr/v1/graphql', // your endpoint.
+  use: [authPlugin, ...defaultPlugins()], // add the auth plugin alongside the default plugins
+})
+</script>
+
 <template>
   <div>
     <NuxtPage />
